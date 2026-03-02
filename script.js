b77b66b81a5952f03e46141344fbb4a547665b76
saurabh suman <saurabhsuman15154@gmail.com>
2026-03-02 23:14:40 +0530

    Add app.js with initial functionality

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("hello world")
+// console.log("hello world!");
+
+// var num = 10;
+// console.log(num);
+// console.log(typeof(num));
+
+
+let a =7;
+let b =5;
+console.log(a+b);
+
+console.log(a-b);
+
+console.log(a*b);
+
+console.log(a/b);
+
+
+
+// const sym1 = Symbol(4);
+// console.log(sym1);
+// const sym2 = Symbol(4);
+// console.log(sym2);
+// if(sym1==sym2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// var i;
+// for(i=0;i<3;i=i+1){
+//     console.log(i);
+// }
+
+// var count;
+// console.log("starting loop" + "<br/>")
+// for(count=0;count<10;count++){
+//     console.log("current count:" +count);
+// }
+
